21142f7a5a527ff2ba72b490c4668ee2c11c7000
like101101 <likelike101101@gmail.com>
2023-05-08 17:12:07 -0400

    added chrome type

--- tests/rise_click.py
+++ tests/rise_click.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+from webdriver_manager.core.utils import ChromeType
 
+driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 
-driver = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()))
 driver.get("https://www.google.com/")
 print(driver.title)
 
