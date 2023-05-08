8447e83b46ae12cb9ef794a8c83fb91cf3ec0127
like101101 <likelike101101@gmail.com>
2023-05-08 16:27:58 -0400

    test print with selenium

--- tests/rise_click.py
+++ tests/rise_click.py
@@ -2,3 +2,6 @@ from selenium import webdriver
 import chromedriver_autoinstaller
 
 chromedriver_autoinstaller.install()
+driver = webdriver.Chrome()
+driver.get('http://github.com')
+print(driver.title)
