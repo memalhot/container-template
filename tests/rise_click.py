5f62228e8b63b3b02bd78bbe3d38991ba207c808
like101101 <likelike101101@gmail.com>
2023-05-08 17:07:11 -0400

    using chrome instead of chromium

--- tests/rise_click.py
+++ tests/rise_click.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chromium.service import ChromiumService
+from selenium.webdriver.chrome.service import Service
 
 
-driver = webdriver.Chrome(service=ChromiumService(executable_path=ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()))
 driver.get("https://www.google.com/")
 print(driver.title)
 
