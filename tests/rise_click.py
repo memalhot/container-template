2b38baba207903d59b0e227323c1f229c9e6a4b5
like101101 <likelike101101@gmail.com>
2023-05-08 16:57:26 -0400

    4th test with driver manager

--- tests/rise_click.py
+++ tests/rise_click.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 
-driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+driver_path = ChromeDriverManager().install()
 driver = webdriver.Chrome(driver_path)
 driver.get("https://www.google.com/")
 print(driver.title)
