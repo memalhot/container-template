bff649b51a8e21a4abe8d4c200790fea27dc56fa
like101101 <likelike101101@gmail.com>
2023-05-08 17:16:24 -0400

    added chrome type

--- tests/rise_click.py
+++ tests/rise_click.py
@@ -2,8 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+
+driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=chrome_options)
 
 driver.get("https://www.google.com/")
 print(driver.title)
